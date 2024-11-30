c52132d51d3f6ae1ed33018dcc7f921b9b12a8c5
musaddiq.siddiqui@outlook.com <musaddiq.siddiqui@outlook.com>
2024-11-30 19:35:05 +0530

    adding tailwind css configuation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+// In src/App.js
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-blue-500 text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome to My Freelance Platform</h1>
     </div>
   );
 }
